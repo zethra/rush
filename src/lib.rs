c2e210932f16c3ccf2f3c7467bd69fafb7c837be
Michael Gattozzi <mgattozzi@gmail.com>
2015-08-09 09:52:05 -0400

    Removed term dependency and temporarily removed clippy dependency until it gets fixed for 1.4
    Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/lib.rs
    	modified:   src/main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 #![feature(str_char)]
 #![feature(path_relative_from)]
 #![feature(custom_derive)]
-#![plugin(clippy)]
+//#![plugin(clippy)]
 #![doc(html_root_url = "https://mgattozzi.github.io/rusty/")]
 pub mod utils;
 pub mod core;
