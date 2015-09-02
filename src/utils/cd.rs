e8aa309c32e4414c91516457a518e63e440a06b6
Michael Gattozzi <mgattozzi@gmail.com>
2015-09-02 11:00:01 -0400

    Fixed cd tests
    Changes to be committed:
    	modified:   src/utils/cd.rs

--- src/utils/cd.rs
+++ src/utils/cd.rs
@@ -58,8 +58,8 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_change_directory_ok(){
-        let vec = vec!["/"];
-        let dir = Path::new("/tmp").to_str();
+        let vec = "/".to_owned();
+        let dir = Path::new("/tmp").to_str().to_owned();
         change_directory(vec);
         let new_dir = env::current_dir()
             .expect("Failed to get current directory");
@@ -69,12 +69,12 @@ mod tests {
 
     #[test]
     fn test_change_directory_fail(){
-        let vec = vec!["/"];
-        let dir = Path::new("/").to_str();
+        let vec = "/".to_owned();
+        let dir = Path::new("/").to_str().to_owned();
         change_directory(vec);
         let new_dir = env::current_dir()
             .expect("Failed to get current directory");
-        let new_dir = new_dir.to_str();
+        let new_dir = new_dir.to_str().to_owned();
         assert_eq!(dir, new_dir);
     }
 
