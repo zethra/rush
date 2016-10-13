069adfa553a55f9c86092f5eca226ab0ceb1a0d8
zethra <jediben97@gmail.com>
2016-10-13 10:31:38 -0400

    Removed clippy due to insatiability

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(plugin)]
 #![feature(custom_derive)]
 #![feature(stmt_expr_attributes)]
-#![plugin(clippy)]
+//#![plugin(clippy)]
 #![doc(html_root_url = "https://mgattozzi.github.io/rusty/")]
 pub mod utils;
 pub mod scripting;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![feature(plugin)]
-#![plugin(clippy)]
+//#![plugin(clippy)]
 
 #![cfg(not(test))]
 
